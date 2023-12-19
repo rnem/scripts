8065f09c943019ce762cec96ac3c4e77ae4ce257
Roger Nem <{ID}+{username}@users.noreply.github.com>
2023-12-19 13:15:20 -0800

    Adding Bash|Shell script files

--- bash_shell/dns_info.sh
+++ bash_shell/dns_info.sh
@@ -15,14 +15,14 @@ while read URL; do
 
     # ISSUE
     if [[ $EXITCODE -ne 0 ]]; then
-		if [[ $EXITCODE -ne 22 ]]; then
-				case $EXITCODE in
-					6) RET="404-Host Not Found" ;; # curl: (6) Couldn't resolve host = 404 - Host not found
-					7) RET="504-Gateway Timeout" ;; # curl: (7) couldn't connect to host = 504 - Gateway timeout
-					28) RET="504-Gateway Timeout" ;;
-					56) RET="504-Gateway Timeout" ;; # curl: (56) Failure when receiving data from the peer = 504 - Gateway timeout
-				esac
-				#echo "issue: $URL - $EXITCODE - $RET"
+        if [[ $EXITCODE -ne 22 ]]; then
+			case $EXITCODE in
+				6) RET="404-Host Not Found" ;; # curl: (6) Couldn't resolve host = 404 - Host not found
+				7) RET="504-Gateway Timeout" ;; # curl: (7) couldn't connect to host = 504 - Gateway timeout
+				28) RET="504-Gateway Timeout" ;;
+				56) RET="504-Gateway Timeout" ;; # curl: (56) Failure when receiving data from the peer = 504 - Gateway timeout
+			esac
+			#echo "issue: $URL - $EXITCODE - $RET"
 		fi
     # ALL OK
     else
@@ -32,14 +32,14 @@ while read URL; do
 
     # RESULT
     if [[ $URL == "www."* ]]; then
-		#CNAME=$(dig NS +short $URL | cut -d' ' -f1 | tr '\n\r' ' ' | sed 's/.net./.net/g' )
-		# tr: remove line breaks, cut values separated by space and get the first value, sed deletes the last character *.com., *.net., etc.
-		CNAME=$(dig CNAME +short $URL | sed 's/.$//' )
-		A=""
+        #CNAME=$(dig NS +short $URL | cut -d' ' -f1 | tr '\n\r' ' ' | sed 's/.net./.net/g' )
+        # tr: remove line breaks, cut values separated by space and get the first value, sed deletes the last character *.com., *.net., etc.
+        CNAME=$(dig CNAME +short $URL | sed 's/.$//' )
+        A=""
     else
-		CNAME=""
-		#A=$(dig $URL +short | sed ':a;N;$!ba;s/\n/,/g' | cut -d' ' -f1 | tr "\n\r" ' ' )
-		A=$(dig $URL +short | tr "\n\r" ',' )
+        CNAME=""
+        #A=$(dig $URL +short | sed ':a;N;$!ba;s/\n/,/g' | cut -d' ' -f1 | tr "\n\r" ' ' )
+        A=$(dig $URL +short | tr "\n\r" ',' )
     fi
 
     #OUTPUT
