74185bcf2d28b146f431bd9936fd3ec1f7ad23f4
Roger Nem <{ID}+{username}@users.noreply.github.com>
2023-12-18 17:22:57 -0800

    Adding PowerShell files

---
+++ powershell/getAD_Info_Computers.ps1
@@ -0,0 +1,21 @@
+###########################################################
+# Extract information of computer from Active Directory   #
+# by Roger Nem (2015)                                     #
+#                                                         #
+# History:                                                #
+# v0.001  - Roger Nem - First Version                     #
+###########################################################
+
+Set-ExecutionPolicy Unrestricted -force
+Get-ChildItem -Path 'E:\AD_Extract\jobs\' -Recurse | Unblock-File
+
+$Headers= @{Label="Name";Expression={$_.Name}},
+@{Label="Distinguished Name";Expression={$_.DistinguishedName}},
+@{Label="Date Created";Expression={$_.Created}}
+
+Get-ADComputer -Filter * -Properties Name,DistinguishedName,Created | Select $Headers | export-csv "E:\AD_Extract\data\AD_Export_Computers.csv" -NoTypeInformation -Encoding UTF8
+
+if ($Host.Name -eq 'ConsoleHost') {
+    Stop-Process $PID
+}
+
