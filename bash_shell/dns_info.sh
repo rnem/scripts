6e66f1f8ad6abf0ed710c154bd3598279256e798
Roger Nem <{ID}+{username}@users.noreply.github.com>
2023-12-19 13:16:29 -0800

    Adding Bash|Shell script files

--- bash_shell/dns_info.sh
+++ bash_shell/dns_info.sh
@@ -16,18 +16,18 @@ while read URL; do
     # ISSUE
     if [[ $EXITCODE -ne 0 ]]; then
         if [[ $EXITCODE -ne 22 ]]; then
-			case $EXITCODE in
-				6) RET="404-Host Not Found" ;; # curl: (6) Couldn't resolve host = 404 - Host not found
-				7) RET="504-Gateway Timeout" ;; # curl: (7) couldn't connect to host = 504 - Gateway timeout
-				28) RET="504-Gateway Timeout" ;;
-				56) RET="504-Gateway Timeout" ;; # curl: (56) Failure when receiving data from the peer = 504 - Gateway timeout
-			esac
-			#echo "issue: $URL - $EXITCODE - $RET"
-		fi
+            case $EXITCODE in
+                6) RET="404-Host Not Found" ;; # curl: (6) Couldn't resolve host = 404 - Host not found
+                7) RET="504-Gateway Timeout" ;; # curl: (7) couldn't connect to host = 504 - Gateway timeout
+                28) RET="504-Gateway Timeout" ;;
+                56) RET="504-Gateway Timeout" ;; # curl: (56) Failure when receiving data from the peer = 504 - Gateway timeout
+            esac
+            #echo "issue: $URL - $EXITCODE - $RET"
+        fi
     # ALL OK
     else
-		RET=$(echo "$OUT" | tail -n1)
-		#echo "ok: $URL: $EXITCODE, $RET"
+        RET=$(echo "$OUT" | tail -n1)
+        #echo "ok: $URL: $EXITCODE, $RET"
     fi
 
     # RESULT
